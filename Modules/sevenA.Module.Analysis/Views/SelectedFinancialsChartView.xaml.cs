34fd9d0d41a470d29662141dd32143e6bf909382
Cesario Almeida <cesario.de.almeida@gmail.com>
2018-01-11 15:38:14 +0800

    separated last 2 remaining views

---
+++ Modules/sevenA.Module.Analysis/Views/SelectedFinancialsChartView.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace sevenA.Module.Analysis.Views
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Media;
+
+    using DevExpress.Xpf.Charts;
+    using DevExpress.Xpf.Grid;
+
+    // ReSharper disable once StyleCop.SA1601
+    public partial class SelectedFinancialsChartView
+    {
+        public SelectedFinancialsChartView()
+        {
+            this.InitializeComponent();
+            DataControlBase.AllowInfiniteGridSize = true;
+        }
+
+        private void OnCustomDrawRatiosPoints(object sender, CustomDrawSeriesPointEventArgs e)
+        {
+            if (!DateTime.TryParseExact(e.SeriesPoint.Argument, "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            {
+                e.DrawOptions.Color = Colors.DarkGoldenrod;
+            }
+        }
+    }
+}
