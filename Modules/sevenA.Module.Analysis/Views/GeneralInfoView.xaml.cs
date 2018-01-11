62fd07ad17074e5b3c3060640318fc71135197e9
Cesario Almeida <cesario.de.almeida@gmail.com>
2018-01-11 14:10:18 +0800

    removed old files; separated generalinfo

---
+++ Modules/sevenA.Module.Analysis/Views/GeneralInfoView.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace sevenA.Module.Analysis.Views
+{
+    using DevExpress.Xpf.Grid;
+
+    // ReSharper disable once StyleCop.SA1601
+    public partial class GeneralInfoView
+    {
+        public GeneralInfoView()
+        {
+            this.InitializeComponent();
+            DataControlBase.AllowInfiniteGridSize = true;
+        }
+    }
+}
